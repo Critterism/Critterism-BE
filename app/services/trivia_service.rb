f6bd33ec6380d1b1c24d1cd965d360e0acc7a829
Nikky Rojas <rojas7313@gmail.com>
2022-11-01 12:41:30 -0600

    add trivia service and service spec

---
+++ app/services/trivia_service.rb
@@ -0,0 +1,16 @@
+class TriviaService
+  class << self
+    def conn
+      Faraday.new('https://opentdb.com')
+    end
+    
+    def parse(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def get_trivia_data
+    response = conn.get("/api.php?amount=50&category=27") 
+    TriviaService.parse(response)[:results]
+    end
+  end
+end
---
+++ spec/services/trivia_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe TriviaService, type: :service do
+  describe 'Class Methods', :vcr do
+    describe '.get_trivia_data' do
+      it 'retrieves trivia q and a data and parses the response', :vcr do
+        parsed_json = TriviaService.get_trivia_data
+        expect(parsed_json).to be_a Array
+        expect(parsed_json[0]).to be_a Hash
+        expect(parsed_json[0][:category]).to be_a String
+        expect(parsed_json[0][:type]).to be_a String 
+        expect(parsed_json[0][:difficulty]).to be_a String  
+        expect(parsed_json[0][:question]).to be_a String  
+      end
+    end
+  end
+end
