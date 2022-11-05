658bc61ed12ac02d061aeb4e58652d09dc576edb
Anna Marie Sterling <sterling.am@gmail.com>
2022-11-05 00:59:55 -0600

    Add line to trivia poro test

--- spec/poros/trivia_spec.rb
+++ spec/poros/trivia_spec.rb
@@ -12,15 +12,17 @@ RSpec.describe Trivia do
         incorrect_answers: ['False']
         }
     end
-    
+
     it 'instantiates described class' do
       trivia = Trivia.new(results)
+
       expect(trivia).to be_instance_of(described_class)
       expect(trivia.category).to eq('Animals')
       expect(trivia.type).to eq('boolean')
       expect(trivia.question).to eq('The axolotl us amphibian that can spend it whole life in a larval state?')
       expect(trivia.correct_answer).to eq('True')
       expect(trivia.incorrect_answers).to eq(['False'])
+      expect(trivia.answers).to eq(["False", "True"])
     end
   end
 end
