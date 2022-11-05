83a56d0c48cbfff01c46b757631d65663891d417
nicole-esquer <nicole.esquer5@gmail.com>
2022-11-05 11:07:51 -0700

    adding filters to vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,8 +68,10 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['everyorg_api_key'] }
+  config.filter_sensitive_data('<api_key>') { ENV['everyorg_api_key'] }
   config.default_cassette_options = { re_record_interval: 7.days }
+  config.allow_http_connections_when_no_cassette = true
+
 end
 
 Shoulda::Matchers.configure do |config|
