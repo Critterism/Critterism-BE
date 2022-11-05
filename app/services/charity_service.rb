cc72af49e5814a5367768373de6941cbea3c483e
nicole-esquer <nicole.esquer5@gmail.com>
2022-11-05 10:48:26 -0700

    one more thing to fix uri issues in circle ci

--- app/services/charity_service.rb
+++ app/services/charity_service.rb
@@ -1,17 +1,17 @@
 class CharityService
   def self.conn
-    Faraday.new(url: 'https://partners.every.org') do |faraday|
+    Faraday.new(url: "https://partners.every.org") do |faraday|
       faraday.params['apiKey'] = ENV['everyorg_api_key']
     end
   end
 
   def self.get_wildlife_charities
-    response = conn.get('/v0.2/browse/wildlife')
+    response = conn.get("/v0.2/browse/wildlife")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.get_animal_charities
-    response = conn.get('/v0.2/browse/animals')
+    response = conn.get("/v0.2/browse/animals")
     JSON.parse(response.body, symbolize_names: true)
   end
 
